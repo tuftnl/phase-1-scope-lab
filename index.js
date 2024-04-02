433f5453e9adec29d756fc47108a3c669f402fc1
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-04-02 19:55:02 -0400

    1??

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = ''
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob'
+}
+
+function setBestCustomer(a = 'not bob') {
+    bestCustomer = a
+}
